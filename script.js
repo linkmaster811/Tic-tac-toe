082a10bc0fa2de69888886505fde1469037a56a4
linkmaster811 <bohmchance@gmail.com>
2023-10-16 16:04:42 +0100

    X's appear on grid

--- script.js
+++ script.js
@@ -5,5 +5,43 @@ form.addEventListener("submit", (event) => {
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
   document.querySelector(".modal-wrapper").setAttribute("Hidden", true);
-  console.log(data);
+  initalizeGame(data);
 })
+
+const initalizeVariables = (data) => {
+  data.choice = +data.choice;
+    data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    data.player1 = "X";
+    data.player2 = "O";
+    data.round = 0;
+    data.currentPlayer = "X";
+    data.gameOver = false;
+}
+
+const addEventListenersToGameBoard = (data) => {
+  document.querySelectorAll(".box").forEach((box) => {
+    box.addEventListener("click", (event) => {
+      playMove(event.target, data);
+    });
+  });
+}
+
+const initalizeGame = (data) => {
+  initalizeVariables(data);
+  addEventListenersToGameBoard(data);
+}
+
+const playMove = (box,data) => {
+  if(data.gameOver) {
+    return;
+  }
+
+  if(data.board[box.id] === "X" || data.board[box.id] === "O"){
+    return;
+  }
+
+  data.board[box.id] = data.currentPlayer
+  box.textContent = data.currentPlayer;
+  box.className = data.currentPlayer === "X" ? "box player1" : "box player2"
+  
+}
